2919a64d6be4bbfc9c4559bd2725ec0862276699
Naru <tristan.etude@gmail.com>
2024-05-10 17:59:13 +0200

    Modification des images de baniere

--- Kasa_Projet_8/src/components/ImageBanner.jsx
+++ Kasa_Projet_8/src/components/ImageBanner.jsx
@@ -6,11 +6,6 @@ export function ImageBanner(props) {
 
   const [currentPicture, setCurrentPicture] = useState(0);
 
-  const getClassName = (i) => {
-    if (i === currentPicture) return "show";
-    return "";
-  };
-
   const moveToNext = () => {
     setCurrentPicture((currentPicture + 1) % pictures.length);
   };
@@ -24,23 +19,31 @@ export function ImageBanner(props) {
     setCurrentPicture(currentPicture - 1);
   };
 
-  const arePicturesAvailable = () => {
-    return pictures && pictures.length > 0;
+  const getCarouselOrDefaultImage = () => {
+    if (pictures && pictures.length > 0) {
+      return pictures.map((pic, i) => (
+        <img key={pic} src={pic} alt="" className={getClassName(i)}></img>
+      ));
+    } else {
+      return (
+        <img
+          src="https://s3-alpha-sig.figma.com/img/56fa/e17e/b9995860bb6384a77ca7dc9bf52da3be?Expires=1716163200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=p9hykQD9K6BC3bN-RJOs7pucd4zqMjJkm-DRTWMXfw3nvoHZ7Y8RbJd-410cVteTDLljzbHyuZX3tqAhJuhKM~dP74nYCwsAOHzvQLv1Mo7MDM6NMny9eBM1KE97agE3Dlhm6o3qvGKdBMizJajy4oDKdvkCwqXSH38Tx0OvFAXt3jOAwPU6KhObez46kXCz8-OlyNQqJQWhsoUX8whJ-fzkWRDyOSiq8-71v92C03RkOe8eTYLNGdfdo42zBJvF8Iq5zLgYIy39vbXmC0mK3n1dv2g4nY5-mgOOhN1eVKbjnHOQbzzVUwCOXaKEf3uBi~RcTduK3w8HvltY3wk0ZQ__" // Remplacez par votre URL d'image par défaut
+          className="show"
+          alt=""
+        />
+      );
+    }
   };
 
-  const getCarouselOrDefaultImage = () => {
-    if (!arePicturesAvailable()) {
-      return <img src="https://picsum.photos/800" className="show" alt="" />;
-    }
-    return pictures.map((pic, i) => (
-      <img key={pic} src={pic} alt="" className={getClassName(i)}></img>
-    ));
+  const getClassName = (i) => {
+    if (i === currentPicture) return "show";
+    return "";
   };
 
   return (
     <div className="image__banner">
       <div className="image__container">{getCarouselOrDefaultImage()}</div>
-      {arePicturesAvailable() && (
+      {pictures && pictures.length > 0 && (
         <>
           <button className="btn btn-previous" onClick={moveToPrevious}>
             <i className="fas fa-chevron-left"></i>
